0042870e73fbc1a364921d92fd0dd34b6e986970
Jeomon <jeogeoalukka@gmail.com>
2024-10-20 12:45:41 +0530

    removed unwanted imports

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from src.inference.gemini import ChatGemini
 from src.agent.web import WebSearchAgent
-from src.message import HumanMessage,SystemMessage
 from dotenv import load_dotenv
 import os
 
@@ -8,7 +7,7 @@ load_dotenv()
 api_key=os.getenv('GOOGLE_API_KEY')
 
 llm=ChatGemini(model='gemini-1.5-pro',api_key=api_key,temperature=0)
-agent=WebSearchAgent(llm=llm,browser='edge',verbose=True,screenshot=False,headless=False)
+agent=WebSearchAgent(llm=llm,browser='edge',verbose=True,strategy='ally_tree',headless=False)
 user_query=input('Enter your query: ')
 agent_response=agent.invoke(user_query)
 print(agent_response)
